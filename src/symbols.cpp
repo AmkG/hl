6f7318c1e497076a728bf1e6aa81b6d30ebd8ec0
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-11-04 21:49:12 +0800

    src/symbols.cpp: expanded table lookup code

--- src/symbols.cpp
+++ src/symbols.cpp
@@ -1,17 +1,25 @@
+#include"all_defines.hpp"
 
 #include"symbols.hpp"
 
 #include<string>
 #include<map>
 
+typedef std::map<std::string, Symbol*> maptype;
+
 Symbol* SymbolsTable::lookup(std::string x) {
-	Symbol*& s = tb[x];
-	if(s) return s;
-	s = new Symbol(x);
-	return s;
+	{/*insert locking of table here*/
+		Symbol*& s = tb[x];
+		if(s) return s;
+		s = new Symbol(x);
+		return s;
+	}
 }
 
 SymbolsTable::~SymbolsTable() {
-	/*go through the*/
+	/*go through the table and delete each symbol*/
+	for(maptype::const_iterator it = tb.begin(); it != tb.end(); ++it) {
+		delete it->second;
+	}
 }
 
