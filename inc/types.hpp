22cb1196a3b6c97a0b4760817bca8fff5147b19f
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-11-06 21:28:30 +0800

    inc/types.hpp: removed assertions, the type is incomplete at that point :(

--- inc/types.hpp
+++ inc/types.hpp
@@ -123,14 +123,6 @@ Base classes for Generic-derived objects
 
 template<class T>
 class GenericDerived : public Generic {
-private:
-	/*compile-time assertion*/
-	/*if this line throws a compile-time error,
-	it means the broken heart is bigger than
-	the actual type T.
-	*/
-	typedef char _check_broken_heart_is_not_bigger_[
-		-1 + 2 * (sizeof(BrokenHeartFor<T>) <= sizeof(T))];
 protected:
 	GenericDerived<T>(void) { }
 public:
@@ -151,13 +143,6 @@ a factory function!
 */
 template<class T>
 class GenericDerivedVariadic : public Generic {
-	/*compile-time assertion*/
-	/*if this line throws a compile-time error,
-	it means the broken heart is bigger than
-	the actual type T.
-	*/
-	typedef char _check_broken_heart_is_not_bigger_[
-		-1 + 2 * (sizeof(BrokenHeartForVariadic<T>) <= sizeof(T))];
 	GenericDerivedVariadic<T>(void); // disallowed!
 protected:
 	/*number of extra Object::ref's*/
