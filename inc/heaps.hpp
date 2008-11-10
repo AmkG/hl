65ad68d29bfd9c9487a1f0747681bc7f330e2ae2
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-11-10 21:51:03 +0800

    inc/heaps.hpp, inc/valueholders.hpp: Moved ValueHolder declaration to heaps.hpp

--- inc/heaps.hpp
+++ inc/heaps.hpp
@@ -3,6 +3,7 @@
 
 #include"objects.hpp"
 #include"generics.hpp"
+#include"valueholders.hpp"
 
 #include<cstring>
 #include<utility>
@@ -58,6 +59,79 @@ public:
 };
 
 /*-----------------------------------------------------------------------------
+ValueHolders
+-----------------------------------------------------------------------------*/
+
+class ValueHolder;
+
+void throw_ValueHolderLinkingError(ValueHolder*);
+
+class ValueHolder {
+private:
+	boost::scoped_ptr<Semispace> sp;
+	Object::ref val;
+
+	/*
+	Allow chaining, for example in
+	the mailbox of a process.
+	*/
+	boost::scoped_ptr<ValueHolder> next;
+
+	ValueHolder() { }
+public:
+	typedef boost::scoped_ptr<ValueHolder> ptr;
+
+	inline size_t used_total(void) const {
+		size_t total = 0;
+		for(ValueHolder const* pt = this; pt; pt = &*pt->next) {
+			total += pt->sp->used();
+		}
+		return total;
+	}
+
+	/* insert(what, to)
+	On entry:
+		what is a pointer to a ValueHolder
+		to is a potentially empty list of
+		  ValueHolder's
+	On exit:
+		what is an empty pointer
+		to is the new list
+	*/
+	static inline void insert(ptr& what, ptr& to) {
+		#ifdef DEBUG
+			/*make sure what to insert exists and doesn't
+			have a next
+			*/
+			if(!what || what->next) {
+				throw_ValueHolderLinkingError(&*what);
+			}
+		#endif
+		what->next.swap(to);
+		what.swap(to);
+	}
+	/* remove(what, from)
+	On entry:
+		what is an empty pointer
+		from is a non-empty list of ValueHolders
+	On exit:
+		what is the first element in the list
+		from is the rest of the list, or empty if the list
+		  only had one element
+	*/
+	static inline void remove(ptr& what, ptr& from) {
+		#ifdef DEBUG
+			/*make sure there's something to remove and that
+			what is empty
+			*/
+			if(!from || what) throw_ValueHolderLinkingError(&*from);
+		#endif
+		what.swap(from);
+		from.swap(what->next);
+	}
+};
+
+/*-----------------------------------------------------------------------------
 Heaps
 -----------------------------------------------------------------------------*/
 
@@ -83,7 +157,7 @@ public:
 		void* pt = main->alloc(sz);
 		try {
 			new(pt) T();
-			return pt;
+			return (T*) pt;
 		} catch(...) {
 			main->dealloc(pt);
 			throw;
