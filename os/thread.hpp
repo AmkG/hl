972e38dbcf5ce6e56776c7c699337e28bc865705
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-11-16 07:03:49 +0800

    os/thread.hpp: Several changes in infrastructure
      Used a static non-member function for launching to_run
      Thread<T> now copies the given to_run; the new thread is
        given that copy and will automatically delete it when
        T::operator() ends.
      Added a RAII class for locking a mutex

--- os/thread.hpp
+++ os/thread.hpp
@@ -1,37 +1,67 @@
 #ifndef THREAD_H
 #define THREAD_H
 
-#include <pthread>
+#include <pthread.h>
+
+#include <boost/noncopyable.hpp>
 
 template <class T>
-class Thread {
+class Thread : boost::noncopyable {
 private:
   pthread_t t;
-  static void* entry(void *to_run);
 public:
-  Thread(T *to_run);
+  Thread(T const& to_run);
 };
 
 template <class T>
-void* Thread::entry(void *to_run) {
-  T *t = (T*)to_run; 
-  (*t)();
+static void* _os_thread_bounce_fun(T *t) {
+  /*
+   * consider using a smart pointer instead of
+   * this dorky try-catch.
+   */
+  try {
+    t();
+    delete t;
+  } catch (...) {
+    delete t;
+    throw;
+  }
+  return NULL;
 }
 
 template <class T>
-Thread::Thread(T *to_run) {
-  pthread_create(&t, NULL, f, to_run);
-  pthread_detach(t);
+Thread::Thread(T const& to_run) {
+  /*
+   * to_run is just used as a template; the new thread is
+   * given a copy, which that thread is responsible for
+   * deleting.
+   * Necessary since we just detach the thread and therefore
+   * cannot easily determine when or if the thread is
+   * already dead, and therefore if we should or shouldn't
+   * delete to_run.
+   */
+  pthread_create(&t, NULL, _os_thread_bounce_fun<T>, new T(to_run));
+  pthread_detach(t); // consider allowing to join
 }
 
-class Mutex {
+class Mutex : boost::noncopyable {
 private:
   pthread_mutex m;
+  void lock() { pthread_mutex_lock(&m); }
+  void unlock() { pthread_mutex_unlock(&m); }
 public:
   Mutex() { pthread_mutex_init(&m, NULL); }
   ~Mutex() { pthread_mutex_destroy(&m); }
-  void lock() { pthread_mutex_lock(&m); }
-  void unlock() { pthread_mutex_unlock(&m); }
+
+  friend class Lock;
+};
+
+class Lock : boost::noncopyable {
+private:
+  Mutex* m;
+public:
+  explicit Lock(Mutex& nm) : m(&nm) { nm.lock(); }
+  ~Lock() { m->unlock(); }
 };
 
 #endif // THREAD_H
