ef77d49c21c17338fef19398920a992074fc7b3e
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-11-17 19:43:52 +0800

    inc/mutexes.hpp: Changed to use os/thread.hpp instead of Boost.Thread

--- inc/mutexes.hpp
+++ inc/mutexes.hpp
@@ -2,7 +2,7 @@
 #define MUTEXES_H
 
 #ifndef single_threaded
-	#include<boost/thread/mutex.hpp>
+	#include"thread.hpp"
 	#include<boost/scoped_ptr.hpp>
 #endif
 #include<boost/noncopyable.hpp>
@@ -13,13 +13,13 @@ class AppTryLock;
 class AppMutex : boost::noncopyable {
 private:
 	#ifndef single_threaded
-		boost::scoped_ptr<boost::try_mutex> mp;
+		boost::scoped_ptr<Mutex> mp;
 	#endif
 public:
 	AppMutex() {
 		#ifndef single_threaded
 			if(!single_threaded) {
-				mp.reset(new boost::try_mutex());
+				mp.reset(new Mutex());
 			}
 		#endif
 	}
@@ -30,14 +30,14 @@ public:
 class AppLock : boost::noncopyable {
 private:
 	#ifndef single_threaded
-		boost::scoped_ptr<boost::try_mutex::scoped_lock > lp;
+		boost::scoped_ptr<Lock> lp;
 	#endif
 	AppLock(); // disallowed!
 public:
 	AppLock(AppMutex& m) {
 		#ifndef single_threaded
 			if(!single_threaded) {
-				lp.reset(new boost::try_mutex::scoped_lock(*m.mp));
+				lp.reset(new Lock(*m.mp));
 			}
 		#endif
 	}
@@ -46,24 +46,26 @@ public:
 class AppTryLock : boost::noncopyable {
 private:
 	#ifndef single_threaded
-		boost::scoped_ptr<boost::try_mutex::scoped_try_lock> lp;
+		boost::scoped_ptr<TryLock> lp;
 	#endif
 	AppTryLock(); //disallowed!
 public:
 	AppTryLock(AppMutex& m) {
 		#ifndef single_threaded
 			if(!single_threaded) {
-				lp.reset(new boost::try_mutex::scoped_try_lock(*m.mp));
+				lp.reset(new TryLock(*m.mp));
 			}
 		#endif
 	}
-	/*returns true if we successfully acquired the lock*/
-	operator bool(void) const {
+	/*safe bool idiom*/
+	inline void unspecified_bool(void) const { }
+	typedef void (AppTryLock::*unspecified_bool_type)(void) const;
+	operator unspecified_bool_type(void) const {
 		#ifndef single_threaded
-			if(!lp) return 1;
-			return lp->locked();
+			if(!lp) return &AppTryLock::unspecified_bool;
+			return (*lp) ? &AppTryLock::unspecified_bool : 0;
 		#else
-			return 1;
+			return &AppTryLock::unspecified_bool;
 		#endif
 	}
 };
