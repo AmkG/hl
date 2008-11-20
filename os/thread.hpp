091ab8a717a32e400bdb884b6d5e22ae4be4cde4
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-11-20 20:18:57 +0800

    os/thread.hpp: Changed behaviour, allowed joining of thread

--- os/thread.hpp
+++ os/thread.hpp
@@ -11,7 +11,9 @@ class Thread : boost::noncopyable {
 private:
   pthread_t t;
 public:
+  void join();
   Thread(T const& to_run);
+  ~Thread();
 };
 
 template <class T>
@@ -22,19 +24,25 @@ static void* _os_thread_bounce_fun(void *v) {
 }
 
 template <class T>
+void Thread<T>::join() {
+  pthread_join(t, NULL);
+}
+
+template <class T>
 Thread<T>::Thread(T const& to_run) {
   /*
    * to_run is just used as a template; the new thread is
    * given a copy, which that thread is responsible for
    * deleting.
-   * Necessary since we just detach the thread and therefore
-   * cannot easily determine when or if the thread is
-   * already dead, and therefore if we should or shouldn't
-   * delete to_run.
+   * The created thread will automatically delete this
+   * itself.
    */
   pthread_create(&t, NULL, _os_thread_bounce_fun<T>, new T(to_run));
-  pthread_detach(t); // consider allowing to join
-  /*alternatively: consider joining on destruction of Thread<T>*/
+}
+
+template <class T>
+Thread<T>::~Thread() {
+  pthread_detach(t);
 }
 
 class Mutex : boost::noncopyable {
