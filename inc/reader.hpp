3141533655de7972964f7d10bcda854f20c3478c
Stefano Dissegna <stefano.dissegna@gmail.com>
2008-12-06 18:53:44 +0100

    reader.hpp: removed useless code

--- inc/reader.hpp
+++ inc/reader.hpp
@@ -4,74 +4,7 @@
 #include <istream>
 #include <string>
 #include "objects.hpp"
-// #include <vector>
-
 #include "symbols.hpp"
-
-// /*
-//  * Intermediate bytecode representation constructed by the reader
-//  * The execution engine should assemble it in a representation suitable
-//  * for execution.
-//  */
-
-// // the argument of a bytecode
-// class BytecodeArg { public: virtual ~BytecodeArg() {}};
-
-// /*
-//  * a simple argument is a value that can fit in a pointer
-//  * it could be an int, a scaled int, an offset, a character, a pointer
-//  * to a Symbol, etc.
-//  */
-// class SimpleArg : public BytecodeArg {
-// private:
-//   Object::ref val;
-// public:
-//   SimpleArg() {}
-//   template <class T>
-//   void setVal(T v) { val = Object::to_ref(v); }
-//   Object::ref getVal() { return val; }
-// };
-
-// // a single bytecode
-// typedef std::pair<Symbol*, BytecodeArg*> bytecode;
-
-// /*
-//  * bytecode sequence
-//  * owns all the bytecode args and frees them
-//  */
-// class BytecodeSeq : public std::vector<bytecode>, public BytecodeArg {
-// public:
-//   ~BytecodeSeq() {}
-// };
-
-// /*
-//  * Argument of bytecode that takes a simple argument and a sequence
-//  */
-// class SimpleArgAndSeq : public BytecodeArg {
-// private:
-//   SimpleArg *sa;
-//   BytecodeSeq *seq;
-// public:
-//   SimpleArgAndSeq(SimpleArg *s, BytecodeSeq *bs) : sa(s), seq(bs) {}
-//   ~SimpleArgAndSeq() {
-//     delete seq;
-//     delete sa;
-//   }
-//   SimpleArg* getSimple() { return sa; }
-//   BytecodeSeq* getSeq() { return seq; }
-// };
-
-// // exception thrown by the reader
-// class ReadError : public std::string {
-// public:
-//   ReadError(const char *str) : std::string(str) {}
-// };
-
-// /*
-//  * Bytecode reader
-//  * extends bc with a bytecode read from in
-//  */
-// static std::istream& operator>>(std::istream & in, BytecodeSeq & bc) { return in; }
 
 class Process;
 // leaves read bytecode on the stack
