96e138bea6c9d74818d86a1147be5128ecec7520
Alan Manuel K. Gloria <almkglor@gmail.com>
2008-12-08 19:32:05 +0800

    inc/all_defines.hpp, src/globals.cpp: Added initalization routine

--- inc/all_defines.hpp
+++ inc/all_defines.hpp
@@ -10,12 +10,18 @@ and should only be included once
 #ifndef DEFINE_GLOBALS
 
 class SymbolsTable;
+class Symbol;
 
 extern boost::scoped_ptr<SymbolsTable> symbols;
+extern Symbol* symbol_sym;
+extern Symbol* symbol_int;
+extern Symbol* symbol_char;
 
 #ifndef single_threaded
 	extern bool single_threaded;
 #endif
+
+void initialize_globals(void);
 
 #endif
 
--- src/globals.cpp
+++ src/globals.cpp
@@ -8,4 +8,14 @@
 #include"symbols.hpp"
 
 boost::scoped_ptr<SymbolsTable> symbols;
+Symbol* symbol_sym;
+Symbol* symbol_int;
+Symbol* symbol_char;
+
+void initialize_globals(void) {
+	symbols.reset(new SymbolsTable());
+	symbol_sym = symbols->lookup("<hl>sym");
+	symbol_sym = symbols->lookup("<hl>int");
+	symbol_sym = symbols->lookup("<hl>char");
+}
 
