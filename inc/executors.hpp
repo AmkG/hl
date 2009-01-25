e0e800ccb6f6ea25a88da1341d364d9bcfdb2b1c
Alan Manuel K. Gloria <almkglor@gmail.com>
2009-01-25 12:59:58 +0800

    inc/bytecodes.hpp: Minor cleanup

--- inc/executors.hpp
+++ inc/executors.hpp
@@ -128,7 +128,7 @@ END_DECLARE_BYTECODES
 
 typedef void* _bytecode_label;
 #define DISPATCH_BYTECODES \
-        bytecode_t *pc = expect_type<Bytecode>(clos->code())->getCode();\
+        bytecode_t *pc = known_type<Bytecode>(clos->code())->getCode();\
 	goto *(pc->op);
 #define NEXT_BYTECODE goto *((++pc)->op)
 #define BYTECODE(x) BYTECODE_ENUM(x); PASTE_SYMBOLS(label_b_, x)
@@ -140,7 +140,7 @@ typedef void* _bytecode_label;
 
 typedef enum _e_bytecode_label _bytecode_label;
 #define DISPATCH_BYTECODES \
-	bytecode_t *pc = expect_type<Bytecode>(clos->code())->getCode();\
+	bytecode_t *pc = known_type<Bytecode>(clos->code())->getCode();\
 	switch(pc->op)
 #define NEXT_BYTECODE {pc++; continue;}
 #define BYTECODE(x) case BYTECODE_ENUM(x)
