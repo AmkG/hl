2a1146b9f287062cc39dcc75cfbab776ed560258
Alan Manuel K. Gloria <almkglor@gmail.com>
2009-02-22 15:07:20 +0800

    tests/types/keys_table_test.cpp: Added nil key test

--- test/types/keys_table_test.cpp
+++ test/types/keys_table_test.cpp
@@ -42,23 +42,46 @@ int main(void) {
 	TestHeap hp(16 * sizeof(Cons));
 	ProcessStack& stack = hp.stack;
 
+	/*empty table*/
 	stack.push( Object::to_ref<Generic*>(hp.create<HlTable>() ));
 	stack.push( stack.top() );
 	HlTable::keys(hp, stack);
 	assert( !stack.top() );
 	stack.pop();
 
+	/*add t key*/
 	stack.push( stack[0] );
 	stack.push( Object::t() );
 	stack.push( Object::t() );
 	HlTable::insert(hp, stack);
 	stack.pop();
 	stack.push( stack.top() );
+	/*should contain just one key, t*/
 	HlTable::keys(hp, stack);
 	assert( stack.top() );
 	assert( maybe_type<Cons>(stack.top()) );
 	assert( is(Object::t(), car(stack.top())) );
 	assert( is(Object::nil(), cdr(stack.top())) );
+	stack.pop();
+
+	/*add nil key*/
+	stack.push( stack[0] );
+	stack.push( Object::t() );
+	stack.push( Object::nil() );
+	HlTable::insert(hp, stack);
+	stack.pop();
+	stack.push( stack.top() );
+	/*should contain two keys, nil and t, in an unspecified order*/
+	HlTable::keys(hp, stack);
+	assert( stack.top() );
+	assert( maybe_type<Cons>(stack.top()) );
+	{Object::ref l2 = cdr(stack.top());
+		assert( l2 );
+		assert( maybe_type<Cons>(l2));
+		assert( !cdr(l2) );
+		assert( is(Object::t(), car(stack.top())) || is(Object::t(), car(l2)) );
+		assert( is(Object::nil(), car(stack.top())) || is(Object::nil(), car(l2)) );
+	}
 	stack.pop();
 
 	std::cout << "Passed!" << std::endl;
