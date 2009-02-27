949a25290ad2dd92dfa97d307401d44d6d48102f
Alan Manuel K. Gloria <almkglor@gmail.com>
2009-02-27 22:59:08 +0800

    inc/aio.hpp: Moved development of new AIO to its own branch, new_aio

--- inc/aio.hpp
+++ inc/aio.hpp
@@ -1,100 +1,126 @@
+#ifndef NEW_AIO_H
+#define NEW_AIO_H
+
+/*Defines virtual AIO objects to be instantiated
+by concrete implementation in the src/ file.
+*/
+
+#include<string>
+#include<stdexcept>
+
+#include<boost/shared_ptr.hpp>
+
+class Process;
+class Symbol;
+
 /*
- * Asynchronous I/O support
- * Abstract classes.
- */
+The "new" AIO is based on event objects.  Event
+objects are just passive requests; in order to
+"arm" an event, it should be added to the global
+event waiting set object.
 
-#ifndef AIO_H
-#define AIO_H
+Events are:
+1.  Write-to-I/O event
+2.  Read-from-I/O event
+3.  Accept-from-I/O Socket event
+4.  Sleep Event
+5.  System Event (i.e. spawn child OS process)
+*/
 
-#include <string>
-#include <queue>
+/*Details are handled by the AIO implementation
+This include file deals only in abstractions;
+we don't care how they are actually implemented.
+*/
 
-// without these my compiler signals an error
-#define INTPTR_MIN		(-2147483647-1)
-#define INTPTR_MAX		(2147483647)
+class Event;
+class ProcessInvoker;
 
-#include "objects.hpp"
+/*Errors in I/O should throw this class*/
+/*Could use a Maybe T type I suppose though*/
+class IOError : public std::runtime_error {
+};
 
-typedef size_t seconds;
+/*-----------------------------------------------------------------------------
+Implementation-specific
+-----------------------------------------------------------------------------*/
 
-class AIOError {
+/*I/O Ports*/
+class IOPort {
+public:
+	void close(void);
+
+	virtual boost::shared_ptr<Event> read(
+		boost::shared_ptr<ProcessInvoker>, size_t
+	) =0;
+	virtual boost::shared_ptr<Event> write(boost::shared_ptr<ProcessInvoker>,
+		std::vector<unsigned char> const&
+	) =0;
+	virtual boost::shared_ptr<Event> accept(
+		boost::shared_ptr<ProcessInvoker>
+	) =0;
+
+	virtual ~IOPort() { }
+};
+
+/*Will only be called once, at init*/
+boost::shared_ptr<IOPort> ioport_stdin(void);
+boost::shared_ptr<IOPort> ioport_stdout(void);
+boost::shared_ptr<IOPort> ioport_stderr(void);
+
+boost::shared_ptr<IOPort> infile(std::string);
+boost::shared_ptr<IOPort> outfile(std::string);
+boost::shared_ptr<IOPort> appendfile(std::string);
+
+class Event {
+public:
+
+	virtual ~Event() { }
+};
+
+boost::shared_ptr<Event> system_event(boost::shared_ptr<ProcessInvoker>,std::string);
+boost::shared_ptr<Event> sleep_event(boost::shared_ptr<ProcessInvoker>,size_t);
+
+/*used as a Singleton, although we don't enforce it*/
+class EventSetImpl;
+class EventSet {
 private:
-  Object::ref err_fn; // function to be called on error
-  std::string err; // error message
+	EventSetImpl* pimpl;
+
 public:
-  AIOError(Object::ref fn, std::string msg) : err_fn(fn), err(msg) {}
-  void invoke(); // invoke the error function with the error message
+	void add_event(boost::shared_ptr<Event>);
+	void remove_event(boost::shared_ptr<Event>);
+
+	EventSet(void);
+	~EventSet();
 };
 
-/*
- * An action to perform when an I/O operation is completed
- */
-class ActionOn {
+/*-----------------------------------------------------------------------------
+Shared across Implementations
+-----------------------------------------------------------------------------*/
+
+/*implementations in src/new_aio.cpp*/
+
+class ProcessInvoker {
+private:
+	Process* P;
 public:
-  virtual ~ActionOn() {}
-  virtual void onComplete(const char *data, size_t len, AIOError *e) = 0;
+	void read_respond(
+		boost::shared_ptr<IOPort>, std::vector<unsigned char> const&
+	);
+	void write_respond(
+		boost::shared_ptr<IOPort>, std::vector<unsigned char> const&
+	);
+	void accept_respond(
+		boost::shared_ptr<IOPort> socket, boost::shared_ptr<IOPort> new_socket
+	);
+	void sleep_respond(
+		boost::shared_ptr<Event>, size_t
+	);
+	void system_respond(
+		boost::shared_ptr<Event>, int term_code
+	);
 };
 
-/*
- * A task is an I/O operation to be scheduled
- */
-class Task {
-public:
-  virtual ~Task() {}
-  virtual bool ready(seconds timeout) = 0;
-  // when perform finishes, it destroys the object
-  virtual void perform() = 0;
-};
 
-/*
- * Specific tasks. The real implementation goes in the backend
- */
-class TaskRead : public Task {};
-class TaskPeek : public Task {};
-class TaskWrite : public Task {};
+#endif // NEW_AIO_H
 
-/*
- * A list of tasks to be performed. The real implementation goes in the backend
- */
-class TaskQueue : public std::queue<Task*> {
-public:
-  virtual ~TaskQueue();
-  virtual void addTask(Task *t) { push(t); }
-  // try to perform every action in the given timeout (for each action)
-  virtual void performAll(seconds timeout);
-};
-
-/*
- * Base async. I/O object
- */
-class AIO {
-public:
-  virtual ~AIO() {}
-  virtual void close() = 0;
-};
-
-class AIN : public AIO {
-public:
-  virtual Task* mkTaskRead(ActionOn *a, size_t how_many) = 0;
-  virtual Task* mkTaskPeek(ActionOn *a) = 0;
-};
-
-class AOUT : public AIO {
-public:
-  virtual Task* mkTaskWrite(ActionOn *a, char *to_write, size_t len) = 0;
-};
-
-/*
- * Abstract file I/O.
- */
-class FileIN : public AIN {
-public:
-  virtual void open(std::string path) = 0;
-};
-
-class FileOUT : public AOUT {
-public:
-  virtual void open(std::string path) = 0;
-};
-
-#endif // AIO_H
