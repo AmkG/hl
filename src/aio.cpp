5efb845315c7f8acab3bfcf22809ca389cbca658
Alan Manuel K. Gloria <almkglor@gmail.com>
2009-03-01 10:13:03 +0800

    src/aio.cpp: added stubs

--- src/aio.cpp
+++ src/aio.cpp
@@ -2,6 +2,8 @@
 
 #include"aio.hpp"
 #include"heaps.hpp"
+#include"types.hpp"
+#include"objects.hpp"
 #include"processes.hpp"
 #include"workers.hpp"
 
@@ -17,4 +19,20 @@ static inline void send_message_to(Process* p, ValueHolderRef& v) {
 	}
 }
 
+ProcessInvoker::ProcessInvoker(Process* nP) : P(nP) {
+	/*TODO: notify workers of one more process-gc root*/
+}
+ProcessInvoker::~ProcessInvoker() {
+	/*TODO: notify workers of loss of root*/
+}
+
+void ProcessInvoker::io_respond(
+		boost::shared_ptr<IOPort> port,
+		std::vector<unsigned char> const& dat) {
+	/*Create a heap to build the response from*/
+	Process hp; ProcessStack& stack = hp.stack;
+	/*build objects*/
+	/*TODO: first define hl-side type for IO port and events*/
+
+}
 
