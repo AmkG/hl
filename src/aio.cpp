2d4689f0b67af2c65e029a3f051aa0819695f37d
Alan Manuel K. Gloria <almkglor@gmail.com>
2009-03-02 20:49:42 +0800

    src/aio.cpp: Corrected several errors

--- src/aio.cpp
+++ src/aio.cpp
@@ -18,18 +18,17 @@ preconditions:
 postconditions:
 	stack.top() has been popped
 */
-static inline void send_message_to(Process* p, ProcessStack& stack) {
+static inline void send_message_to(Process* P, ProcessStack& stack) {
 	/*prepare message*/
 	ValueHolderRef m;
 	ValueHolder::copy_object(m, stack.top());
-	send_message_to(P, m);
 	stack.pop();
 
 	bool is_waiting = 0;
 	/*keep sending until we definitely go through any locks or whatnot*/
-	while(!p->receive_message(v, is_waiting)) /*do nothing*/;
+	while(!P->receive_message(m, is_waiting)) /*do nothing*/;
 	if(is_waiting) {
-		workers->workqueue_push(p);
+		workers->workqueue_push(P);
 	}
 }
 
@@ -53,9 +52,8 @@ void ProcessInvoker::io_respond(
 	if(!dat || dat->size() == 0) {
 		stack.push(Object::nil());
 	} else {
-		BinObj* e = hp.create<BinObj>();
-		e->p.swap(dat);
-		stack.push(Object::to_ref<Generi*>(e));
+		BinObj* e = BinObj::create(hp, dat);
+		stack.push(Object::to_ref<Generic*>(e));
 	}
 	bytecode_cons(host, stack);
 
