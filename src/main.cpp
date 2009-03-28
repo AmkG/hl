a5fba9d8c685b1a4d4ca768470e792f8901adf10
Stefano Dissegna <stefano.dissegna@gmail.com>
2009-03-28 09:10:27 +0100

    sample main

---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+/*
+ * Example hl driver -- execute bytecode
+ */
+#include "all_defines.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+#include "reader.hpp"
+#include "executors.hpp"
+#include "symbols.hpp"
+#include "types.hpp"
+
+using namespace std;
+
+void throw_HlError(const char *str) {
+  cout << "Error: " << str << endl << flush;
+  exit(1);
+}
+
+void throw_OverBrokenHeart(Generic*) {
+  throw_HlError("overbrokenheart");
+}
+
+void usage() {
+  cout << "hl file1 [file2 ...]" << endl;
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    usage();
+    return 1;
+  }
+
+  initialize_globals();
+
+
+  for (int i = 1; i < argc; i++) {
+    ifstream in(argv[i]);
+    if (!in) {
+      cerr << "Can't open file: " << argv[i] << endl;
+      return 2;
+    }
+    
+    Process p;
+    Process* Q;
+    size_t timeslice;
+    timeslice = 128;
+    execute(p, timeslice, Q, 1); // init phase
+
+    read_sequence(p, in);
+    assembler.go(p);
+    Closure *k = Closure::NewKClosure(p, 0);
+    k->codereset(p.stack.top()); p.stack.pop();
+    p.stack.push(Object::to_ref(k)); // entry point
+    execute(p, timeslice, Q); // run!
+    
+    cout << p.stack.top() << endl;
+  }
+
+  return 0;
+}
