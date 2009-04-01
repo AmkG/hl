d8b3cc6a176c2690b476693e45fe8dabd4e59db8
Dissegna Stefano [IN1] <sdissegn@dip144.studenti.math.unipd.it>
2009-04-01 11:50:07 +0200

    catch std::bad_alloc

--- src/executors.cpp
+++ src/executors.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h> // for size_t
 #include <string>
 #include <sstream>
+#include <new> // for std::bad_alloc
 #include "types.hpp"
 #include "executors.hpp"
 #include "bytecodes.hpp"
@@ -1200,26 +1201,27 @@ ProcessStatus execute(Process& proc, size_t& reductions, Process*& Q, bool init)
     // or nil if there was an error
     BYTECODE(spawn): {
 	    AllWorkers &w = AllWorkers::getInstance();
-	    // create new process 
-	    Process *spawned = new Process();
-	    if (spawned == NULL) {
-		    // couldn't allocate process
-		    stack.push(Object::nil());
-	    } else {
-		    // register process to working queue
-		    w.register_process(spawned);
-		    w.workqueue_push(spawned);
-		    // set starting function
-		    spawned->stack.push(stack.top()); stack.pop();
-		    // release cpu as soon as possible
-		    // we can't just return process_running or process_change
-		    // because we can't resume execution in the middle of a 
-		    // function and <bc>spawn is not required to appear in tail
-		    // position
-		    reductions = 0;
-		    HlPid *pid = proc.create<HlPid>();
-		    pid->process = spawned;
-		    stack.push(Object::to_ref(pid));
+	    try {
+	      // create new process 
+	      Process *spawned = new Process();
+	      // register process to working queue
+	      w.register_process(spawned);
+	      w.workqueue_push(spawned);
+	      // set starting function
+	      spawned->stack.push(stack.top()); stack.pop();
+	      // release cpu as soon as possible
+	      // we can't just return process_running or process_change
+	      // because we can't resume execution in the middle of a 
+	      // function and <bc>spawn is not required to appear in tail
+	      // position
+	      reductions = 0;
+	      HlPid *pid = proc.create<HlPid>();
+	      pid->process = spawned;
+	      stack.push(Object::to_ref(pid));
+	    }
+	    catch (std::bad_alloc e) {
+	      // couldn't allocate process
+	      stack.push(Object::nil());
 	    }
     } NEXT_BYTECODE;
     BYTECODE(string_create): {
