d6764b52fa3c9c68bd216e7bce9701bf5af8a0bc
Alan Manuel K. Gloria <almkglor@gmail.com>
2009-04-04 21:59:38 +0800

    src/executors.cpp: Comments regarding process_waiting.
      If the process gets set to status process_waiting, it *CANNOT*
        be touched afterwards, because it's possible for a race to
        develop where a worker makes the process process_waiting
        (which implicitly pushes it back into the queue), get
        suspended, then another worker wakes up that process and
        start fooling around with its state, get suspended itself,
        then the original worker fools around with the process.

--- src/executors.cpp
+++ src/executors.cpp
@@ -1038,7 +1038,11 @@ ProcessStatus execute(Process& proc, size_t& reductions, Process*& Q, bool init)
       } else {
         std::cerr<<"recv: queue empty\n";
         // <bc>recv is always called in tail position
-	// !! doesn't work: process will restart from the beginning !!
+        // !! NOTE!  If Process::extract_message() above ever
+        // !! returns false, we should not change anything in the
+        // !! process!
+        // !! IN PARTICULAR: we cannot leave any Process::ExtraRoot
+        // !! objects hanging around.
         return process_waiting;
       }
     } NEXT_BYTECODE;
