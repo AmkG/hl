091484fe20a06a6abebaf2424ee9e51ca7263fb4
Alan Manuel K. Gloria <almkglor@gmail.com>
2009-04-06 13:14:05 +0800

    inc/workers.hpp: Corrected declaration

--- inc/workers.hpp
+++ inc/workers.hpp
@@ -125,7 +125,7 @@ public:
 	size_t T;
 
 	/*callable, used to launch a thread*/
-	void operator(bool is_main=0)(void);
+	void operator()(bool is_main=0);
 
 	explicit Worker(AllWorkers* nparent)
 		: parent(nparent),
