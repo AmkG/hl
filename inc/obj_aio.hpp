b340c35da4b2dac88fcb4d47050a58c7ed7b5bb4
Alan Manuel K. Gloria <almkglor@gmail.com>
2009-04-11 18:28:15 +0800

    inc/obj_aio.hpp: factored out some bits that will get repeated in other I/O functions

--- inc/obj_aio.hpp
+++ inc/obj_aio.hpp
@@ -51,30 +51,66 @@ inline Object::ref only_running(Process& P) {
 		/*otherwise*/				Object::nil() ;
 }
 
-static inline Object::ref io_accept(Process& host, Object::ref proc, Object::ref port) {
+static inline boost::shared_ptr<ProcessInvoker> create_process_invoker(
+		Object::ref proc ) {
 	#ifdef DEBUG
 		if(!maybe_type<HlPid>(proc)) {
-			throw_HlError("accept: Expected a process");
+			throw_HlError("I/O: Expected a process");
 		}
+	#endif
+	return boost::shared_ptr<ProcessInvoker>(
+		new ProcessInvoker(
+			known_type<HlPid>(proc)->process
+		)
+	);
+}
+
+static inline Object::ref create_event(
+		Process& host,
+		Object::ref proc,
+		boost::shared_ptr<Event>& event) {
+	HlEvent* hp = host.create<HlEvent>();
+	hp->hl_pid = proc;
+	hp->p = event;
+	return Object::to_ref<Generic*>(hp);
+}
+
+static inline Object::ref create_io_port(
+		Process& host,
+		boost::shared_ptr<IOPort>& port) {
+	HlIOPort* hp = host.create<HlIOPort>();
+	hp->p = port;
+	return Object::to_ref<Generic*>(hp);
+}
+
+static inline Object::ref handle_io_error(Process& host, IOError& err) {
+	std::string errmsg = err.what();
+	ProcessStack& stack = host.stack;
+	/*for now, assume error messages are purely ASCII*/
+	for(size_t i = 0; i < errmsg.size(); ++i) {
+		host.stack.push(Object::to_ref(UnicodeChar(errmsg[i])));
+	}
+	HlString::stack_create(host, host.stack, errmsg.size());
+	HlTagged* hp = host.create<HlTagged>();
+	hp->o_type = Object::to_ref(symbol_io);
+	hp->o_rep = stack.top(); stack.pop();
+	return Object::to_ref<Generic*>(hp);
+}
+
+inline Object::ref io_accept(Process& host, Object::ref proc, Object::ref port) {
+	#ifdef DEBUG
 		if(!maybe_type<HlIOPort>(port)) {
 			throw_HlError("accept: Expected an I/O port");
 		}
 	#endif
 	boost::shared_ptr<IOPort> now_accept;
-	boost::shared_ptr<ProcessInvoker> pi(
-		new ProcessInvoker(
-			known_type<HlPid>(proc)->process
-		)
-	);
+	boost::shared_ptr<ProcessInvoker> pi = create_process_invoker(proc);
 	try {
 		boost::shared_ptr<Event> event =
 			known_type<HlIOPort>(port)->p->accept(pi, now_accept)
 		;
 		if(event) {
-			HlEvent* hp = host.create<HlEvent>();
-			hp->hl_pid = proc;
-			hp->p = event;
-			return Object::to_ref<Generic*>(hp);
+			return create_event(host, proc, event);
 		} else {
 			#ifdef DEBUG
 				if(!now_accept) {
@@ -84,22 +120,10 @@ static inline Object::ref io_accept(Process& host, Object::ref proc, Object::ref
 					);
 				}
 			#endif
-			HlIOPort* hp = host.create<HlIOPort>();
-			hp->p = now_accept;
-			return Object::to_ref<Generic*>(hp);
+			return create_io_port(host, now_accept);
 		}
 	} catch(IOError& err) {
-		std::string errmsg = err.what();
-		/*for now, assume error messages are purely ASCII*/
-		for(size_t i = 0; i < errmsg.size(); ++i) {
-			host.stack.push(Object::to_ref(UnicodeChar(errmsg[i])));
-		}
-		HlString::stack_create(host, host.stack, errmsg.size());
-		HlTagged* hp = host.create<HlTagged>();
-		hp->o_type = symbol_io;
-		hp->o_rep = stack.top(); stack.pop();
-		return Object::to_ref<Generic*>(hp);
-		/*TODO*/
+		return handle_io_error(host, err);
 	}
 }
 
